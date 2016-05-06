5a4bb22e83397fa318e9a02cf59aab5e4115c8c2
L.Laddie <liuhan132@foxmail.com>
2016-05-06 10:20:41 +0800

    reapir

--- source/course.h
+++ source/course.h
@@ -5,18 +5,34 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <map>
 
 #include "user.h"
 
 class Course
 {
 public:
+	Course(std::string id, std::string name, std::string credit);
+	~Course();
+
+	std::string getID();					
+	std::string getName();
+	int getCredit();		
+
+	Teacher* getTeacher();		
+	void setTeacher(Teacher* teacher);
+
+	std::set<Student*> getStudent();
+	void addStudent(Student* student);
+
+	std::map<Student*, float> getStudentGrade();	//获取当前课程学生信息
+
 	virtual calculateGPA(float myGrade);	//计算绩点
 
 private:
-	std::string _ID;		//课程编号
-	std::string _name;		//课程名称
-	std::string _credit;	//课程学分
-	std::string _num;		//课程人数
-	Student* _student;		//选课学生
+	std::string m_ID;						//课程编号
+	std::string m_name;						//课程名称
+	int m_credit;							//课程学分
+	std::map<Student*, float> m_student;	//选课学生与对应成绩
+	Teacher* m_teacher;						//授课教师
 };
--- source/user.h
+++ source/user.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <set>
 
 #include "course.h"
 
@@ -12,48 +13,48 @@
 class User
 {
 public:
-	User();
+	User(std::string name, std::string id, std::string insititude);
 	~User();
 
  	std::string getID();
-	void setID(std::string arg);
-
 	std::string getName();
-	void setName(std::string arg);
-
 	std::string getInsititude();
-	void setInsititude(std::string arg);
 
 private:
-	std::string _name;			//用户姓名
-	std::string _ID;			//用户学号或者教工号
-	std::string _insititude;	//用户所属学院
+	std::string m_name;			//用户姓名
+	std::string m_ID;			//用户学号或者教工号
+	std::string m_insititude;	//用户所属学院
 };
 
 //由User类派生出学生子类，代表学生用户
 class Student: public User
 {
 public:
-	Student();
+	using User::User;
 	~Student();
 
 	std::string getClass();
-	void setClass();
+	void setClass(std::string class);
+
+	std::map<Course*, float> getGrade();
+	std::set<Course*> getCourse();
 
 private:
-	std::string _class;		//学生所属班级
+	std::string m_class;					//学生所属班级
+	std::set<Course*> m_course;				//学生对应课程和成绩
 };
 
 //由User类派生出教师子类，代表教师用户
 class Teacher : public User
 {
 public:
-	Teacher();
+	using User::User;
 	~Teacher();
 
 	Course getCourse();
-	void setCourse(Course arg);
+
+	void setCourseGrade(Course* course, Student* student, float grade);
 
 private:
-	Course* _course;	//教师任课课程
+	std::set<Course*> m_course;	//教师任课课程
 };
